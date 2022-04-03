c564024bda1973276ac4cb5b64b536bfbddc61e8
libin <1790572759@qq.com>
2022-04-03 12:41:48 +0800

    消除代码复制

--- CNN.py
+++ CNN.py
@@ -11,17 +11,14 @@ from torch.autograd import Variable
 import torch.utils.data as data_utils
 import torch.nn.functional as F
 # import torch.optim as optim
-
+#定义神经网络模型
 class MyCNN(nn.Module):
     def __init__(self):
         super(MyCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, padding=2)
         self.conv2 = nn.Conv2d(16, 1, kernel_size=5, padding=2)
-
         self.max_pool8 = torch.nn.MaxPool2d(8, 8)
-        
         self.fc1 = nn.Linear(300, 1)
-
     def forward(self, x):
         out = F.relu(self.conv1(x))
         out = F.relu(self.conv2(out))
@@ -30,76 +27,35 @@ class MyCNN(nn.Module):
         #print(out.size())
         #ps = raw_input()
         out = torch.sigmoid(self.fc1(out))
-
         return out
-X_train = np.zeros((1748, 300, 300, 3))
-Y_train = np.zeros((1748, 1))
-
-filepath_train=r'd:\tempworkspace\python_workspace\ship_heading_detection\train_data\\'
-train_image= os.listdir(filepath_train)
-# print(type(train_image))
-i = 0
-for item in train_image:
-    # print(type(item))
-    #break
- 
-    image_train = io.imread(filepath_train+item)
-    X_train[i]=image_train
-    angle_train=item.split("_ang_")[1]
-    angle_train=angle_train.split(".jpg")[0]
-    Y_train[i]=float(angle_train)
-    i=i+1
-X_train = np.swapaxes(X_train,1,3)
-
-
-
-X_test = np.zeros((1228, 300, 300, 3))
-Y_test = np.zeros((1228, 1))
-
-filepath_test=r'd:\tempworkspace\python_workspace\ship_heading_detection\test_data\\'
-test_image= os.listdir(filepath_test)
-# print(type(train_image))
-j = 0
-for item in test_image:
-    # print(type(item))
-    #break
- 
-    image_test = io.imread(filepath_test+item)
-    X_test[j]=image_test
-    angle_test=item.split("_ang_")[1]
-    angle_test=angle_test.split(".jpg")[0]
-    Y_test[j]=float(angle_test)
-    j=j+1
-X_test = np.swapaxes(X_test,1,3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#加载数据
+def loadData(filePath, sampleSize, pixelSize, channels):
+    X = np.zeros((sampleSize, pixelSize, pixelSize, channels))
+    Y = np.zeros((sampleSize, 1))
+    image = os.listdir(filePath)
+    i = 0
+    for item in image:
+        # print(type(item))
+        #break
+        img = io.imread(filePath+item)
+        X[i]=img
+        angle=item.split("_ang_")[1]
+        angle=angle.split(".jpg")[0]
+        Y[i]=float(angle)
+        i=i+1
+    X = np.swapaxes(X,1,3)
+    return X,Y
+#加载训练集
+X_train,Y_train = loadData(r'd:\tempworkspace\python_workspace\ship_heading_detection\train_data\\', 100, 300, 3)
+#加载测试集
+X_test,Y_test = loadData(r'd:\tempworkspace\python_workspace\ship_heading_detection\test_data\\', 100, 300, 3)
 
 train = data_utils.TensorDataset(torch.from_numpy(X_train).float(), torch.from_numpy(Y_train).float())
 test = data_utils.TensorDataset(torch.from_numpy(X_test).float(), torch.from_numpy(Y_test).float())
 train_loader = data_utils.DataLoader(train, batch_size=1, shuffle=True)
 test_loader = data_utils.DataLoader(test, batch_size=1, shuffle=False)
 
-
-
 print("train:"+str(type(train))+" test:"+str(type(test))+" train_loader:"+str(type(train_loader))+" test_loader:"+str(type(test_loader)))
-
-
-
-
 
 net = MyCNN()
 criterion = nn.MSELoss()
